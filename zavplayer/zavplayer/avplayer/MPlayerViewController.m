c3d84b09468c5aa069a43a182a2bbc81390cf05a
朱鹏飞 <411294962@qq.com>
2017-11-03 18:59:30 +0800

    add nstime

--- zavplayer/zavplayer/avplayer/MPlayerViewController.m
+++ zavplayer/zavplayer/avplayer/MPlayerViewController.m
@@ -16,6 +16,7 @@
 @interface MPlayerViewController ()
 
 @property(nonatomic) bool isplaying;
+@property (nonatomic) long long last_load_bytes;
 - (void)play:(id)sender;
 - (void)pause:(id)sender;
 - (void)stop:(id)sender;
@@ -55,6 +56,9 @@ static void *AVPlayerDemoPlaybackViewControllerStatusObservationContext = &AVPla
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.last_load_bytes = 0;
+    [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(myTimeObserve) userInfo:nil repeats:true];
+    
     [self prepareToPlay];
 
     self.isplaying = false;
@@ -72,11 +76,14 @@ static void *AVPlayerDemoPlaybackViewControllerStatusObservationContext = &AVPla
     PlayerView* playview = [[PlayerView alloc]initWithFrame:rect_play];
     //UIColor* black = [UIColor colorWithRed:0 green:0 blue:0 alpha:1];
 
-    //[self.myXibView addSubview:playview];
     [self.myXibView insertSubview:playview atIndex:0];
-    //[self.myXibView showControl];
-    //[self.myXibView bringControlViewToFront];
     [playview setPlayer:self.mPlayer];
+    
+    
+//    NSTimer *looptime = [NSTimer timerWithTimeInterval:1 target:self selector:@selector(myTimeObserve) userInfo:nil repeats:true];
+//    NSRunLoop *loop = [NSRunLoop currentRunLoop];
+//    [loop addTimer:looptime forMode:NSDefaultRunLoopMode];
+    
     //[playview setBackgroundColor:black];
     // Do any additional setup after loading the view, typically from a nib.
 }
@@ -125,7 +132,7 @@ static void *AVPlayerDemoPlaybackViewControllerStatusObservationContext = &AVPla
     //ad
     //http://mobaliyun.res.mgtv.com/new_video/2017/10/27/1021/5FB9EAC11551779E3C7DA7DF5E9BAF41_20171027_1_1_666.mp4
     //
-    NSURL *url = [[NSURL alloc]initWithString:@"http://pcvideoyf.titan.mgtv.com/c1/2017/11/02_0/16ABC431F58120C330D71837D739FA13_20171102_1_1_2842_mp4/6AD59933ABA98F4BD287911AA29B49EC.m3u8?arange=0&pm=PnxdyoZwM8VcT1kCa_zGlf53pxHOPesVB6QGVpFrL14l3hJAnIQzaHpMea3jgIWOTSMkuXF3UpP~0c2~Eb0LiawAS3jE3VHmH98BRjRi5M7v3FlMEj4QhezmcYeQo~k6KsPOWTmO_NvNamjkUvZa6Uqr1NiOAb5twXdzsqNQKPSEkXxZIjdekpNUkf9RRdzhWdpotXkrsJyri5dyR1b7BM9LFc8T7Reg_HNmmf2Ph52E_TUSHXBtRmeMj9v88tXTC3HJC837HYqR74YvzI5efJnHzh23~BduYA0D_EVk9n0OBN3kDWQ203ErCLVR2mPAD1gzBdW7b2E6rdKGOjE6YDjhspgq5U3vvT_flspCA082UyaUGzlL8~KaYDaVgSQbL7VGrTOAxkaN~_uBtTGFpj~U~WYAI9HF0c026SZNP9R_uEWamsxyq2cAfNE-"];
+    NSURL *url = [[NSURL alloc]initWithString:@"http://pcvideoyf.titan.mgtv.com/c1/2017/11/02_0/16ABC431F58120C330D71837D739FA13_20171102_1_1_2842_mp4/6AD59933ABA98F4BD287911AA29B49EC.m3u8?arange=0&pm=o9XgymnfhPLzGudNdfsJGzn_AIDbxtpK5gI1BLYAGl9Fx9B7PA6qJ0~1x7kglGn3hNKKYdwiHa59znSe_JOqUxqoQNL~qk~IEBgDoHaLlenvGL8V78GkoWZPf090kjTXC02Rk0BFzli0YTbN0pdJ~ugNRsQ6fyHJBbB5ZvViSTUdkePyaUx5B3ZpY3KnRrNalUGO~F7WcJyYRgbw3IKLumNsRtpQXMPC5LvUObtt7hxBhbtq_L~jFNOz5NBwfJkAxy6DneuVpsSfwFL22LMdoTAC66yEwk9r~aOLqmggMmNwoRVFgcLHC7bHxw~AEXrcxZTT7jE2bwyJLA4k2CvdZd5_0EO89aaHxXRi9JxrZ3LkK7HSuT4oogfEDQ1d7Um5WXd0EiOyN_1gpUAO6mpzP0fzjnK8zl1y49giDRhT6LDjRr97sERFjKn5RBI-"];
     mAsset = [AVURLAsset URLAssetWithURL:url options:nil];
     NSArray *assetKeys = @[@"playable", @"hasProtectedContent"];
     mPlayerItem = [AVPlayerItem playerItemWithAsset:mAsset automaticallyLoadedAssetKeys:assetKeys];
@@ -235,10 +242,10 @@ static void *AVPlayerDemoPlaybackViewControllerStatusObservationContext = &AVPla
     else if([keyPath isEqualToString:@"playbackLikelyToKeepUp"]){
         NSNumber *newvalue = change[NSKeyValueChangeNewKey];
         NSNumber *oldvalue = change[NSKeyValueChangeOldKey];
-        
+        NSLog(@"playbackLikelyToKeepUp");
         bool newkeekup = [newvalue boolValue];
         bool oldkeekup = [oldvalue boolValue];
-        if((newkeekup != oldkeekup) && (self.playstate == ZPlayerStatusSuspend)){
+        if((newkeekup == 1) && (self.playstate == ZPlayerStatusSuspend)){
             self.playstate = ZPlayerStatusPlaying;
             [self.myXibView showProgressView:false];
         }
@@ -343,4 +350,27 @@ static void *AVPlayerDemoPlaybackViewControllerStatusObservationContext = &AVPla
 -(void)movieDidPlayToEndTime{
     NSLog(@"movieDidPlayToEndTime");
 }
+
+-(void)myTimeObserve{
+    NSLog(@"my time observe");
+    //get speed
+    if(self.mPlayerItem != nil){
+        AVPlayerItemAccessLog *access_log = self.mPlayerItem.accessLog;
+        
+        NSArray<AVPlayerItemAccessLogEvent *> *event =  access_log.events;
+        
+        long long loadbyte = event.firstObject.numberOfBytesTransferred;
+        if(self.last_load_bytes > 0){
+            long long currsec_load_bytes = loadbyte - self.last_load_bytes;
+            self.last_load_bytes = loadbyte;
+            NSLog(@"current bitrate %0.6f KB/s", currsec_load_bytes/1024.0);
+        }
+        else
+            self.last_load_bytes = loadbyte;
+        //display speed
+    }else{
+        self.last_load_bytes = 0;
+    }
+}
+
 @end
